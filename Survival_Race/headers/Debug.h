0117b6feec7fcb7588e6713be01d9c7ecad17b02
HanChu <leonardo.chu@icloud.com>
2020-06-30 11:01:12 +0200

    Add Debug

---
+++ Survival_Race/headers/Debug.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Road.h"
+
+void print_index(int index, int x, int y);
+void print_current_speed(Road road, int x, int y);
+void print_current_segment(Road road, int x, int y);
+void print_next_segment(Road road, int x, int y);
+void print_all_segment(Road road, int x, int y);
