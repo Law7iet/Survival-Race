758daf270dcf22f30f6d56198555c6d3ad2dff30
HanChu <45383691+leochuhan@users.noreply.github.com>
2020-07-07 20:49:16 +0200

    Delete Locate.cpp

--- Locate.cpp
+++
@@ -1,9 +0,0 @@
-#include "../headers/Locate.h"
-
-#include <Windows.h>
-
-void Locate(int x, int y)
-{
-	COORD point = { (SHORT)x, (SHORT)y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), point);
-}
