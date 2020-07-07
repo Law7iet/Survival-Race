2ffded47eaf6370a7440392d23b216bf5b9d199a
fellinggood <61427676+fellinggood@users.noreply.github.com>
2020-07-07 17:09:44 +0200

    Update Locate.cpp

--- Survival_Race/src/Locate.cpp
+++ Survival_Race/src/Locate.cpp
@@ -2,7 +2,7 @@
 
 #include <Windows.h>
 
-void Locate(int x, int y)
+void Locate(int x, int y)           //Posizionamento del cursore
 {
 	COORD point = { (SHORT)x, (SHORT)y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), point);
