a935d0a7089199b5e201d9fff06300524d665434
HanChu <leonardo.chu@icloud.com>
2021-03-20 15:44:10 +0100

    Refactoring

--- Survival-Race/headers/Debug.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Road.h"
-
-void print_index(int index, int x, int y);
-void print_current_speed(Road road, int x, int y);
-void print_current_segment(Road road, int x, int y);
-void print_next_segment(Road road, int x, int y);
-void print_all_segment(Road road, int x, int y);
--- Survival-Race/headers/Locate.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// positioning of the cursor in coordinate (x, y)
-void Locate(int x, int y);
